3e3d374f4244392bb0a455fea6fb07b62e696b51
minsuk1 <palt1230@naver.com>
2022-07-12 16:16:46 +0900

    Update sample2.py

--- streamlit/sample2.py
+++ streamlit/sample2.py
@@ -25,7 +25,8 @@ df=pd.DataFrame(rows)
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
-st.area_chart(chart_data)
+#st.area_chart(chart_data)
+st.area_chart(rows)
 
 # Print results.
 for row in rows:
