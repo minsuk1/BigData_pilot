3d0839e7ec5ba1583fe3298fa51530c200d43867
minsuk1 <palt1230@naver.com>
2022-07-12 17:34:14 +0900

    Update sample2.py

--- streamlit/sample2.py
+++ streamlit/sample2.py
@@ -19,14 +19,6 @@ def run_query(query):
 
 rows = run_query("SELECT * from gu;")
 
-import pandas as pd
-import numpy as np
-df=pd.DataFrame(rows)
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-#st.area_chart(chart_data)
-st.area_chart(rows)
 
 # Print results.
 for row in rows:
