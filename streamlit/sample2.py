1abcf61afeec195775ad33b8ed9d28862c1419a3
minsuk1 <palt1230@naver.com>
2022-07-12 10:32:50 +0900

    Update sample2.py

--- streamlit/sample2.py
+++ streamlit/sample2.py
@@ -17,7 +17,7 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from mytable;")
+rows = run_query("SELECT * from gu;")
 
 # Print results.
 for row in rows:
